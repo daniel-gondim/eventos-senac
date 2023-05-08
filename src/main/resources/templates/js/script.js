d52807602c283781108ede863b89a7917ad21eb1
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-05-08 15:44:10 -0300

    adicionando javascript para salvar dados do formulário de equipamentos

--- src/main/resources/templates/js/script.js
+++ src/main/resources/templates/js/script.js
@@ -1,3 +1,8 @@
 const formulario = document.querySelector("novo-form");
+const InputDescricao = document.querySelector("equipo-descricao");
+const InputObservacao = document.querySelector("equipo-observacao");
 
-console.log(formulario);
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(InputDescricao.value);
+});

36bd622ecd04fd38f8ef6cc1c78cab6cc91ddded
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-05-09 12:48:28 -0300

    adicionando métodos salvar os dados do formulário e limpar ao clicar no botão salvar

--- src/main/resources/templates/js/script.js
+++ src/main/resources/templates/js/script.js
@@ -1,8 +1,31 @@
-const formulario = document.querySelector("novo-form");
-const InputDescricao = document.querySelector("equipo-descricao");
-const InputObservacao = document.querySelector("equipo-observacao");
+const formulario = document.querySelector(".novo-form");
+const InputDescricao = document.querySelector(".equipo-descricao");
+const InputObservacao = document.querySelector(".equipo-observacao");
+
+function salvar() {
+    fetch("http://localhost:8080/equipamento/create",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            method: "POST",
+            body: JSON.stringify({
+                descricao: InputDescricao.value,
+                observacao: InputObservacao.value
+            })
+        })
+        .then(function (res) {console.log(res) })
+        .catch(function (res) {console.log(res) })
+};
+
+function limpar() {
+    InputDescricao.value = "",
+        InputObservacao.value = ""
+}
 
 formulario.addEventListener('submit', function(event) {
     event.preventDefault();
-    console.log(InputDescricao.value);
+    salvar();
+    limpar();
 });

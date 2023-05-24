ac7f972511ba1e526d684b8a444d4db70b12bcc3
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-05-24 13:46:28 -0300

    refactor: corrige html e deleta funções sem uso no js

--- src/main/resources/templates/js/script.js
+++ src/main/resources/templates/js/script.js
@@ -1,4 +1,5 @@
-const formulario = document.querySelector(".novo-form");
+const itens = [];
+const formulario = document.getElementById("novo-form");
 const InputDescricao = document.querySelector(".equipo-descricao");
 const InputObservacao = document.querySelector(".equipo-observacao");
 
@@ -15,34 +16,28 @@ function salvar() {
                 observacao: InputObservacao.value
             })
         })
-        .then(function (res) {console.log(res) })
-        .catch(function (res) {console.log(res) })
-        // window.location.replace("http://localhost:8080/equipamentos");
-        window.location.replace("../templates/equipamentos.html");
-}
-
+        .then(function (res) {
+            if (res.ok) {
+                console.log(res)
+                window.location.replace("http://localhost:8080/equipamentos")
+                window.location.href = "../templates/exibir_equipamentos.html";
+                //window.location.replace("../templates/exibir_equipamentos.html");
+            } else {
+                console.log("Erro ao salvar o equipamento");
+            }
+        })
+        .catch(function (res) {
+            console.log("Erro na requisição:", res);
+//            console.log(res)  })
+        });
+};
 function limpar() {
     InputDescricao.value = "",
-    InputObservacao.value = ""
+        InputObservacao.value = ""
 }
-
-// suposto método para adicionar itens na tabela tirado do código do Rafael
-function salvarItem() {
-    var itemInput = document.getElementById('itemInput');
-    var itemText = itemInput.value;
-
-    if (itemText !== '') {
-        var itemList = document.getElementById('itemList');
-        var novoItem = document.createElement('li');
-        novoItem.textContent = itemText;
-        itemList.appendChild(novoItem);
-
-        itemInput.value = '';
-    }
-
-    formulario.addEventListener('submit', function(event) {
-        event.preventDefault();
-        salvar();
-        limpar();
-    });
-}
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault()
+    salvar()
+    limpar();
+    mostraLista();
+});

2399dbff78338e6ac88fa5688fac269bd938bb6e
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-05-24 21:30:32 -0300

    iniciando a lógica para que cada equipamento seja exibido com um botão 'Editar'

--- src/main/resources/templates/js/exibir_equipamentos.js
+++ src/main/resources/templates/js/exibir_equipamentos.js
@@ -10,7 +10,38 @@ fetch('http://localhost:8080/equipamentos') // substitua com a URL da sua API
         data.forEach(equipo => {
             const listEquipo = document.createElement('li'); // cria linha para armazenar descrição e observação do equipamento
             const equipoInfo = `${equipo.descricao} - ${equipo.observacao}`; // Concatenando descrição e observação
-            listEquipo.textContent = equipoInfo; // atribui à variável o conteúdo de texto de equipoInfor
+            listEquipo.textContent = equipoInfo;
+
+            const editButton = document.createElement('button');
+            editButton.textContent = 'Editar';
+            listEquipo.appendChild(editButton);
+
+            let isEditMode = false; // variável para controlar o modo de edição
+            editButton.addEventListener('click', () => {
+                if (isEditMode) {
+                    // Salvar as alterações
+                    equipo.descricao = inputEquipoDescricao.value;
+                    equipo.observacao = inputEquipoObservacao.value;
+                    listEquipo.textContent = `${equipo.descricao} - ${equipo.observacao}`;
+                    editButton.textContent = 'Editar';
+                } else {
+                    // Habilitar o modo de edição
+                    const inputEquipoDescricao = document.createElement('input');
+                    inputEquipoDescricao.value = equipo.descricao;
+                    listEquipo.insertBefore(inputEquipoDescricao, editButton);
+
+                    const inputEquipoObservacao = document.createElement('input');
+                    inputEquipoObservacao.value = equipo.observacao;
+                    listEquipo.insertBefore(inputEquipoObservacao, editButton);
+
+                    editButton.textContent = 'Salvar';
+                }
+
+                isEditMode = !isEditMode; // Alternar o modo de edição
+            });
+            listEquipo.textContent = equipoInfo; // atribui à variável o conteúdo de texto de equipoInfo
+
+            // atribui à variável o conteúdo de texto de equipoInfor
             lista.appendChild(listEquipo);
         });
     })
